6fa777101dde51e82c84d40ed4801d42e3e3f9be
Jhon Carlo Palatino <111470308+Carlo06@users.noreply.github.com>
2023-05-11 10:11:49 +0800

    Add files via upload

---
+++ exercise3/MyDog/chiuhuahua.java
@@ -0,0 +1,7 @@
+package MyDog;
+interface chiuhuahua{
+
+    String bark = "Japan";
+
+    void bark();
+}
---
+++ exercise3/MyDog/shihuahua.java
@@ -0,0 +1,9 @@
+package MyDog;
+
+class shihuahua extends shitzu implements chiuhuahua{
+
+
+    public void bark(){
+        System.out.println("My pomeranian is barking");
+    }
+}
